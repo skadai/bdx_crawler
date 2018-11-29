56e59c813fff9519629c620ff30515e5f5725c4f
skadai <skchang08@126.com>
2018-11-29 19:11:26 +0800

    fix

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ from setuptools import setup
 
 
 def readme():
-    with open('README.md', 'r', encoding='utf-8') as f:
+    with open('README.md', 'r') as f:
         return f.read()
 
 
 def load_require():
-    with open('requirements.txt', 'r', encoding='utf-8') as f:
+    with open('requirements.txt', 'r') as f:
         return f.read().split('\n')
 
 
